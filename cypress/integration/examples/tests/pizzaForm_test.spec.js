0a10393effa92716158819803190671fac528793
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2020-11-13 22:04:03 -0500

    stuck-on-clickLink

--- cypress/integration/examples/tests/pizzaForm_test.spec.js
+++ cypress/integration/examples/tests/pizzaForm_test.spec.js
@@ -1,32 +1,55 @@
 /// <reference types="cypress" />
 
-describe("Quotes app", () => {
+describe("build pizza App", () => {
     beforeEach(() => {
       // arbitrary code you want running before your tests start: setup
       cy.visit("http://localhost:3000");
     });
 
     const nameInput = () => cy.get('input[name="name"]');
-    const specialInput = () => cy.get('input[name="specialIns"]');
-    const addOrderbtnInput = () => cy.get('#addOrderBtn');
+    // const specialInput = () => cy.get('input[name="specialIns"]');
+    const addOrderBtn = () => cy.get('#addOrderBtn');
+
+    const pepperoniInput = () => cy.get('input[name="pepperoni"]');
+    const italianSaugeInput = () => cy.get('input[name="italianSauge"]');
+    const onionsInput = () => cy.get('input[name="onions"]');
+    const extraChesseInput = () => cy.get('input[name="extraCheese"]');
+
     const homeLink = () => cy.get("#homeLink");
     const formLink = () => cy.get("#formLink");
+    const OrderBtn = () => cy.get("#addOrderBtn");
 
 
     it("tests working", () => {
         expect(1 + 2).to.equal(3);
     });
 
-    it("can click form link", () => {
-      formLink()
-      .click()
-    });
+    // it("can click form link", () => {
+    //   formLink()
+    //   .click()
+    // });
 
     it("type name text", () => {
       nameInput()
       .should("have.value", "")
       .type("tania")
       .should("have.value", "tania")
+    });
+
+    it("can select toppings", () => {
+        pepperoniInput()
+        .check()
+        italianSaugeInput()
+        .check()
+        onionsInput()
+        .check()
+        extraChesseInput()
+        .check()
+    });
+
+    it("can submit order", () => {
+        OrderBtn()
+        .click()
     });
 
 });
